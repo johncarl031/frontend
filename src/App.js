0da0b1a1c6b4fef2fef9f62e80243e706a84609f
uidn4075 <john.carl.landrito@continental-corporation.com>
2018-12-21 09:59:46 +0800

    Modified travisci file again2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload. You're Awesome!
+            Edit <code>src/App.js</code> and save to reload. You're Awesome JC! Superb !
           </p>
           <a
             className="App-link"
